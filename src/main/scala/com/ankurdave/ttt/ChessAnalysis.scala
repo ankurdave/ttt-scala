a53a2ab70252fbcec19c45fd63864f830586845e
Ankur Dave <ankurdave@gmail.com>
2018-11-17 01:42:25 -0800

    Add file loader

---
+++ src/main/scala/com/ankurdave/ttt/ChessAnalysis.scala
@@ -0,0 +1,25 @@
+package com.ankurdave.ttt
+
+import scala.io.Source
+import java.time.YearMonth
+
+object ChessAnalysis extends App {
+  val file = args(1)
+  val source = Source.fromFile(file)
+  val ms = 
+    for {
+      line <- source.getLines
+      fields = line.split(",")
+      dateInt = fields(1).toInt
+      month = dateInt % 12
+      year = (dateInt - month) / 12
+      winner = fields(2)
+      loser = fields(3)
+    } yield (Date(YearMonth.of(year, month + 1)), PlayerId(winner), PlayerId(loser))
+  val matches = new Matches(ms.toSeq)
+  source.close()
+
+  for (((d, p), skill) <- new TTT(matches).run().skillVariables.toSeq.sortBy(_._1._1.ym)) {
+    println("%s,%d,%f,%f".format(p, d.ym.getYear * 12 + d.ym.getMonthValue, skill.mu, skill.sigma))
+  }
+}
